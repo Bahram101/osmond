bc98dab0dca56ba33715d3510137ccacaa2d36b1
Bahrom <bahram8787@gmail.com>
2025-11-07 12:24:00 +0500

    add vercel postgres

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,15 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NEXT_PUBLIC_API_URL ||
-  (typeof window !== "undefined"
-    ? window.location.origin
-    : "http://localhost:3000/api");
-
 export const instance = axios.create({
-  baseURL: baseURL,
-  withCredentials: true,
-  // headers: {
-  //   "Content-Type": "application/json",
-  // },
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "/api",
+  withCredentials: true, 
 });
