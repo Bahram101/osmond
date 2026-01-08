889c5d5fd0fb8ccf1740144ca3bc4a3e7eead11b
Bahrom <bahram8787@gmail.com>
2026-01-08 12:09:49 +0500

    set cat tree ...

--- src/app/api/categories/tree/route.ts
+++ src/app/api/categories/tree/route.ts
@@ -1,15 +1,16 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+//GET /api/categories/tree
+
 type CategoryNode = {
   id: number;
   name: string;
   parentId: number | null;
-  children: CategoryNode[]
-}
+  children: CategoryNode[];
+};
 
 export async function GET() {
-
   const categories = await prisma.category.findMany({
     select: {
       id: true,
@@ -17,25 +18,60 @@ export async function GET() {
       parentId: true,
     },
     orderBy: {
-      name: 'asc'
+      id: "asc",
+    },
+  });
+
+  const byId: Record<number, CategoryNode> = {};
+  const tree: CategoryNode[] = [];
+
+  // console.log("categories", categories);
+
+  // const map = new Map<number, CategoryNode>();
+
+  // categories.forEach((cat) => {
+  //   map.set(cat.id, { ...cat, children: [] });
+  // });
+
+  // const tree: CategoryNode[] = [];
+
+  // console.log("map", map);
+
+  // map.forEach((cat) => {
+  //   console.log("cat", cat);
+  // });
+
+  for (const cat of categories) {
+    // 1. Создаём текущую категорию, если её ещё нет
+    if (!byId[cat.id]) {
+      byId[cat.id] = {
+        id: cat.id,
+        name: cat.name,
+        parentId: cat.parentId,
+        children: [],
+      };
     }
-  })
-  console.log('categories', categories);
 
-  const map = new Map<number, CategoryNode>();
+    // 2. Если есть родитель — добавляем в children родителя
+    if (cat.parentId !== null) {
+      // создаём родителя, если он ещё не создан
+      if (!byId[cat.parentId]) {
+        byId[cat.parentId] = {
+          id: cat.parentId,
+          name: "",
+          parentId: null,
+          children: [],
+        };
+      }
 
-  categories.forEach(cat => {
-    map.set(cat.id, { ...cat, children: [] })
-  })
+      byId[cat.parentId].children.push(byId[cat.id]);
+    } else {
+      // 3. Корневая категория
+      tree.push(byId[cat.id]);
+    }
 
-  const tree: CategoryNode[] = []
-
-  console.log('map', map);
-
-  map.forEach(cat => {
-    console.log('cat', cat);
-  })
-
-
+    console.log('byId',JSON.stringify(byId, null, 2));
+    console.log("tree", JSON.stringify(tree, null, 2));
+  }
   return NextResponse.json({ message: "Not Implemented" }, { status: 501 });
 }
