05666b859d93d6915ab9a811665ce365277fc1cb
Bahrom <bahram8787@gmail.com>
2026-02-20 11:05:46 +0500

    remove google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
 import QueryProvider from "@/providers/QueryProvider";
 import { Toaster } from "sonner";
 import AuthProvider from "@/providers/AuthProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Osmond",
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-200`}
+        className={`antialiased bg-zinc-200`}
       >
         <AuthProvider>
           <QueryProvider>{children}</QueryProvider>
